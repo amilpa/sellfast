9e8a228f2fd2254aae7f54949014aab84e3ffb0e
amilpa <amilpa2020@gmail.com>
2023-05-22 21:30:28 +0530

    nav setup

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import SignInButton from "./SignInButton";
-import SignOutButton from "./SignOutButton";
+import ProfileIcon from "./ContextMenu/ProfIleIcon";
+import ContextMenu from "./ContextMenu/ContextMenu";
 
 export default async function Navbar() {
   const session = await getServerSession(authOptions);
   return (
     <div className="fixed top-0 w-full flex justify-between py-4 border-b-[1px] border-b-[rgba(255,255,255,0.2)]">
       <span>
-        <span className="absolute top-6 left-7 w-0 h-0 border-[15px] border-transparent border-t-solid border-t-white"></span>
-        <h1 className="text-2xl ml-16 font-semibold tracking-wider">
+        <span className="absolute top-[25px] md:top-6 left-3 md:left-7 w-0 h-0 border-[15px] border-transparent border-t-solid border-t-white"></span>
+        <h1 className="text-2xl ml-11 md:ml-16 font-semibold tracking-wider">
           SellFast
         </h1>
       </span>
-      {session ? <SignOutButton /> : <SignInButton />}
+      {session ? (
+        <ProfileIcon session={session}>
+          <ContextMenu />
+        </ProfileIcon>
+      ) : (
+        <SignInButton />
+      )}
     </div>
   );
 }
---
+++ src/app/components/ProfIleIcon.jsx
@@ -0,0 +1,13 @@
+"use client";
+import Image from "next/image";
+
+export default function ProfileIcon({ session }) {
+  return (
+    <Image
+      src={session.user.image}
+      width={75}
+      height={30}
+      className="rounded-[50%] m-auto cursor-pointer"
+    />
+  );
+}
