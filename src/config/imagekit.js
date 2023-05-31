c602af5c1a9db18baba70fb86247016313412c61
amilpa <amilpa2020@gmail.com>
2023-05-31 21:15:42 +0530

    imagekit config

---
+++ src/config/imagekit.js
@@ -0,0 +1,8 @@
+
+var Imagekit = require('imagekit-javascript')
+
+export var imagekit = new Imagekit({
+  publicKey: "public_fN/CtGIOdAg+gjxmqZ3318Fcnmg=",
+  privateKey: "private_eSVBmWYfvXxe2l+GvIJStzEkD+U=",
+  urlEndpoint: "https://ik.imagekit.io/jfbwocie1/"
+})
