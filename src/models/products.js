e4ce5c75e1824dedb16d3a96e8f9aefc5406329a
amilpa <amilpa2020@gmail.com>
2023-06-09 23:51:36 +0530

    add get one

--- src/models/products.js
+++ src/models/products.js
@@ -6,3 +6,27 @@ export async function getAllProducts() {
   const data = await pool.query("SELECT * from products");
   return data.rows;
 }
+
+//add product
+export async function addProduct(product) {
+  const data = await pool.query(
+    "INSERT INTO products(product_id,name,description,imageurl,price,user_id) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+    [
+      product.product_id,
+      product.name,
+      product.description,
+      product.imageurl,
+      product.price,
+      product.user_id,
+    ]
+  );
+  return data.rows[0];
+}
+
+//get product by id
+export async function getProductById(id) {
+  const data = await pool.query("SELECT * from products WHERE product_id=$1", [
+    id,
+  ]);
+  return data.rows[0];
+}
