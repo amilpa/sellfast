d1b036037dabc9f0495486307f593fec14efe2b4
amilpa <amilpa2020@gmail.com>
2023-06-22 22:19:15 +0530

    try middle

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,16 @@
+import { NextResponse } from "next/server";
+// import checkAuth from "./utils/checkAuth";
 
-import fileParse from "./middlewares/fileParse";
-
-export default function middleware(request) {
-  // fileParse()
+export default async function middleware(req) {
+  // const url = new URL(req.url)
+  // if (!session) {
+  //   if (url.pathname.includes("/api") && !url.pathname.includes("auth"))
+  // return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  // }
+  // return NextResponse.json({ message: "Sucessful" })
 }
+
+// match everything except /api/auth
+export const config = {
+  matcher: '/api/user/:path*'
+};
